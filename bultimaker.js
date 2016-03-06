f01082c73255a00a3ba1742c7191a9250b8e4183
Philippe Vanhaesendonck <AmedeeBulle@users.noreply.github.com>
2016-03-06 22:23:00 +0100

    Initial commit

---
+++ bultimaker.js
@@ -0,0 +1,236 @@
+//
+//  Bul(le)timaker
+//
+//  Build Ultimaker firmware
+//  AngularJS App
+//
+
+/*global angular, Blob */
+var bultimakerApp = angular.module('bultimakerApp', []);
+
+// Allow blob URLs
+bultimakerApp.config(['$compileProvider', function ($compileProvider) {
+  "use strict";
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|blob):/);
+}]);
+
+// UMO Factory to initialize data and talk to the server
+// We don't really need a factory here -- just for educational purpose
+bultimakerApp.factory('umoFactory', ['$http', function ($http) {
+  "use strict";
+  var umo = {},                     // Factory object
+    baseUrl = 'bultimaker.php',     // Base server URL
+
+    profiles = [                    // Machine profiles
+      {
+        label: 'Ultimaker Original',
+        buildParams: {
+          target:     'UMO'
+        }
+      },
+      {
+        label: 'Ultimaker Original with HBK',
+        buildParams: {
+          target:       'HBK',
+          tempBed:      20        // Heated bed si PT100
+        }
+      },
+      {
+        label: 'Ultimaker Original +',
+        buildParams: {
+          target:       'UMOP',
+          motherboard:  72,         // UMOP has UM2 board
+          temp0:        20,         // Extruder is PT100
+          temp1:        20,         // 2nd extruder if any is PT100
+          tempBed:      20          // Heated bed si PT100
+        }
+      }
+    ],
+
+    defaultParams = {                 // Default parameters for all machines
+      motherboard:    7,              // Default UMO Board
+      extruders:      1,              // Single extruder
+      temp0:          -1,             // Extruder is thermocouple
+      temp1:          -1,             // 2nd extruder if any is thermocouple
+      tempBed:        0,              // No heated bed
+      controller:     'Ulti',         // Ulticontroller
+      displayFan:     0,              // Display Fan% on Ulticontroller
+      baudrate:       250000,         // Default baud rate
+      invertX:        1,              // Default axis direction on UMO
+      invertY:        0,
+      language:       1,              // Interface language -- English
+      timeout:        15000,          // Display timeout
+      pulleys:        0,              // 0: MXL, 1: GT2
+      maxtemp0:       275,            // Max temp for extruder 0
+      maxtemp1:       275,            // Max temp for extruder 1
+      beep:           'Ulti',         // Beep type. Ultimaker traditional or Marlin original
+      reverseEncoder: 0               // To fix the rotary knob
+    };
+
+  // Returns available profiles
+  umo.getProfiles = function () {
+    return profiles;
+  };
+
+  // Returns default parameters
+  umo.getParams = function () {
+    return defaultParams;
+  };
+
+  umo.getGitInfo = function () {
+    return $http.post(baseUrl, {cmd: 'info'});
+    // return $http.get('bultimakerInfo.json');
+  };
+
+  umo.buildFirmware = function (params) {
+    params.cmd = 'build';             // Inject command
+    return $http.post(baseUrl, params);
+    // return $http.get('bultimakerBuild.json');
+  };
+
+  return umo;
+}]);
+
+bultimakerApp.controller('bultimakerCtrl', function ($scope, umoFactory) {
+  "use strict";
+
+  // List of values
+  $scope.lovSensor = [
+    { key: -2,   descr: 'Thermocouple with MAX6675 (only for sensor 0)'},
+    { key: -1,   descr: 'Thermocouple with AD595'},
+    { key: 0,    descr: 'None'},
+    { key: 1,    descr: '100k thermistor - best choice for EPCOS 100k (4.7k pullup)'},
+    { key: 2,    descr: '200k thermistor - ATC Semitec 204GT-2 (4.7k pullup)'},
+    { key: 3,    descr: 'Mendel-parts thermistor (4.7k pullup)'},
+    { key: 4,    descr: '10k thermistor !! do not use it for a hotend. It gives bad resolution at high temp. !!'},
+    { key: 5,    descr: '100K thermistor - ATC Semitec 104GT-2 (Used in ParCan & J-Head) (4.7k pullup)'},
+    { key: 6,    descr: '100k EPCOS - Not as accurate as table 1 (created using a fluke thermocouple) (4.7k pullup)'},
+    { key: 7,    descr: '100k Honeywell thermistor 135-104LAG-J01 (4.7k pullup)'},
+    { key: 71,   descr: '100k Honeywell thermistor 135-104LAF-J01 (4.7k pullup)'},
+    { key: 8,    descr: '100k 0603 SMD Vishay NTCS0603E3104FXT (4.7k pullup)'},
+    { key: 9,    descr: '100k GE Sensing AL03006-58.2K-97-G1 (4.7k pullup)'},
+    { key: 10,   descr: '100k RS thermistor 198-961 (4.7k pullup)'},
+    { key: 11,   descr: '100k beta 3950 1% thermistor (4.7k pullup)'},
+    { key: 12,   descr: '100k 0603 SMD Vishay NTCS0603E3104FXT (4.7k pullup) (calibrated for Makibox hot bed)'},
+    { key: 20,   descr: 'The PT100 circuit found in the Ultimainboard V2.x'},
+    { key: 60,   descr: '100k Maker\'s Tool Works Kapton Bed Thermistor beta=3950'},
+    { key: 51,   descr: '100k thermistor - EPCOS (1k pullup)'},
+    { key: 52,   descr: '200k thermistor - ATC Semitec 204GT-2 (1k pullup)'},
+    { key: 55,   descr: '100k thermistor - ATC Semitec 104GT-2 (Used in ParCan & J-Head) (1k pullup)'},
+    { key: 1047, descr: 'PT1000 with 4k7 pullup'},
+    { key: 1010, descr: 'PT1000 with 1k pullup (non standard)'},
+    { key: 147,  descr: 'PT100 with 4k7 pullup'},
+    { key: 110,  descr: 'PT100 with 1k pullup (non standard)'}
+  ];
+  $scope.lovBaudrate = [
+    { key: 250000,   descr: '250000 (Default)'},
+    { key: 115200,   descr: '115200'}
+  ];
+  $scope.lovController = [
+    { key: 'Ulti',   descr: 'UltiController (or no controller)'},
+    { key: 'FGSC',   descr: 'RepRapDiscount Full Graphic Smart Controller'}
+  ];
+  $scope.lovLanguage = [
+    { key: 1,   descr: 'English'},
+    { key: 3,   descr: 'Français'}
+  ];
+  $scope.lovTimeout = [
+    { key:  5000,   descr: ' 5 sec.'},
+    { key: 10000,   descr: '10 sec.'},
+    { key: 15000,   descr: '15 sec. (Default)'},
+    { key: 20000,   descr: '20 sec.'},
+    { key: 30000,   descr: '30 sec.'},
+    { key: 45000,   descr: '45 sec.'},
+    { key: 60000,   descr: ' 1 min.'}
+  ];
+  $scope.lovPulleys = [
+    { key: 0,   descr: 'MXL (Standard UMO)'},
+    { key: 1,   descr: 'GT2 (Upgrade)'}
+  ];
+  $scope.lovTemp = [
+    { key: 275,   descr: '275°C (Default)'},
+    { key: 280,   descr: '280°C'},
+    { key: 285,   descr: '285°C'},
+    { key: 290,   descr: '290°C'},
+    { key: 295,   descr: '295°C'},
+    { key: 300,   descr: '300°C'},
+    { key: 305,   descr: '305°C'},
+    { key: 310,   descr: '310°C'},
+    { key: 315,   descr: '315°C'}
+  ];
+  $scope.lovBeep = [
+    { key: 'Ulti',   descr: 'UltiController default'},
+    { key: 'Marlin', descr: 'Marlin original'}
+  ];
+  
+  // Buttons state and fields initialization
+  $scope.disableCompile = false;
+  $scope.disableDownload = true;
+  $scope.buildLog = '';
+  $scope.buildMd5 = '';
+  $scope.buildFirmware = '';
+  $scope.firmwareFile = '';
+  $scope.compileError = false;
+  $scope.httpError = false;
+
+  // Compile button
+  $scope.compile = function () {
+    // Disable compile button and clear previous displays
+    $scope.disableCompile = true;
+    $scope.disableDownload = true;
+    $scope.buildLog = "";
+    $scope.buildMd5 = "";
+    $scope.buildFirmware = "";
+    $scope.compileError = false;
+    $scope.httpError = false;
+
+    umoFactory.buildFirmware($scope.params).
+      success(function (data, status, headers, config) {
+        if (data.status === 0 && data.firmware) {
+          // All good, we (should) have a firmware
+          var firmwareBlob = new Blob([data.firmware], { type : 'application/octet-stream' });
+          $scope.buildFirmware = (window.URL || window.webkitURL).createObjectURL(firmwareBlob);
+          $scope.buildLog = data.log;
+          $scope.buildMd5 = data.md5;
+          $scope.firmwareFile = 'MarlinUltimaker-' + $scope.params.target +
+            '-' + $scope.params.baudrate +
+            ($scope.params.extruders === 2 ? '-dual' : '') +
+            '.hex';
+          $scope.disableDownload = false;
+        } else {
+          $scope.buildLog = data.log;
+          $scope.compileError = true;
+        }
+      }).
+      error(function (data, status, headers, config) {
+        $scope.httpErrorData = data;
+        $scope.httpErrorStatus = status;
+        $scope.httpError = true;
+      })['finally'](function () {   // use this notation to make JSLint happy...
+        // Call done, re-enable compile button
+        $scope.disableCompile = false;
+      });
+  };
+
+  // Get data from factory
+  $scope.infoError = '';
+  $scope.info = {};
+  umoFactory.getGitInfo().
+    success(function (data, status, headers, config) {
+      if (data.status === 0) {
+        $scope.info = data;
+        $scope.info.repo = $scope.info.url.replace('https://github.com/', '');
+      } else {
+        $scope.infoError = data.log || 'Cannot retrieve source information from server';
+      }
+    }).
+    error(function (data, status, headers, config) {
+      $scope.infoError = 'Cannot retrieve source information from server';
+    });
+  $scope.profiles = umoFactory.getProfiles();
+  $scope.umSelect = $scope.profiles[0];
+  $scope.params = angular.extend({}, umoFactory.getParams(), $scope.umSelect.buildParams);
+  $scope.umSelected = function () {
+    $scope.params = angular.extend({}, umoFactory.getParams(), $scope.umSelect.buildParams);
+  };
+});
---
+++ bultimaker.php
@@ -0,0 +1,155 @@
+<?php
+//
+//  Bul(le)timaker
+//
+//  Build Ultimaker firmware
+//  Server side in PHP
+//
+
+// Site config
+require_once 'bultimaker.inc';
+
+// Constants
+define("GIT"            , "git --git-dir=".MARLIN."/.git");   // Git command
+define("BUILD_PREFIX"   , "Marlin_");                         // Prefix for build dir
+define("FIRMWARE"       , "Marlin.hex");                      // Firmware file name
+
+// We always answer in JSON
+header('Content-Type: application/json');
+
+// Response array
+$resp = array(
+  'status' => -1, 
+  'log' => 'Invalid request', 
+);
+
+// Decode json input as array
+$data = json_decode(file_get_contents('php://input'), true);
+
+if ( !isset($data) || json_last_error() != JSON_ERROR_NONE )
+  goto exit_json;  // Invalid JSON
+
+// Sanitize input
+foreach ($data as $key => $value) {
+  $data[$key] = preg_replace('/[^a-z0-9-_.+:]/i', '', $value);
+}
+
+if ( !isset($data['cmd']))
+  goto exit_json;  // No request
+
+switch ($data['cmd']) {
+  case "info":
+    // Returns info on the git repo
+    $resp['url']          = exec(GIT.' config --get remote.origin.url');
+    $resp['branch']       = exec(GIT.' rev-parse --abbrev-ref HEAD');
+    $resp['hash']         = exec(GIT.' log -1  --format="%H"');
+    $resp['shortHash']    = exec(GIT.' log -1  --format="%h"');
+    $resp['tag']          = exec(GIT.' describe --tags --abbrev=0');
+    $resp['authorDate']   = exec(GIT.' log -1  --format="%ar"');
+    $resp['subject']      = shell_exec(GIT.' log -1  --format="%s"');
+    $resp['experimental'] = strpos($resp['branch'], 'Experimental') === FALSE ? 0 : 1;
+    $resp['log']          = '';
+    $resp['status']       = 0;
+    break;
+  case "build":
+    // build the firmware
+    // We have currently the following parameters:
+    //  target, motherboard, extruders, temp0, temp1, tempBed, baudrate, controller, language
+  
+    // Create unique directory for the build
+    $buildDir = sys_get_temp_dir() . '/' . uniqid(BUILD_PREFIX);
+    if(! mkdir($buildDir) ) {
+      $resp['log'] = 'Cannot create build directory';
+      goto exit_json;
+    }
+  
+    // Generate the command line
+
+    // Generic make parameters
+    $cmd = 'make --no-print-directory ' .
+      '-C "'                  . MARLIN                . '/Marlin" ' .
+      'ARDUINO_INSTALL_DIR="' . ARDUINO_PATH          . '" ' .
+      'ARDUINO_VERSION="'     . ARDUINO_VERSION       . '" ' .
+      'AVR_TOOLS_PATH="'      . ARDUINO_PATH          . '/hardware/tools/avr/bin/" ' .
+      'BUILD_DIR="'           . $buildDir             . '" ' .
+      'HARDWARE_MOTHERBOARD=' . $data['motherboard']  . ' ';
+    // Add UG8Lib for the Full Graphic
+    if ($data['controller'] == 'FGSC') {
+      $cmd .= 'U8GLIB=1 ';
+    }
+
+    // Defines
+    $cmd .= 'DEFINES="'.
+      'VERSION_BASE=\'\\"'    . ($data['motherboard'] == 7 ? 'Ultimaker:' : 'Ultimaker+:') . 
+                                exec(GIT.' describe --tags --abbrev=0 | sed -e "s!UMOU_!!"') . '\\"\'' .
+      ' VERSION_PROFILE=\'\\"'. $data['baudrate'] . ($data['extruders'] == 1 ? '_single' : '_dual') . '\\"\'' .
+      ' BAUDRATE='            . $data['baudrate'] . 
+      ' EXTRUDERS='           . $data['extruders'] . 
+      ' TEMP_SENSOR_BED='     . $data['tempBed'] . 
+      ' TEMP_SENSOR_0='       . $data['temp0'] .
+      ' TEMP_SENSOR_1='       . ($data['extruders'] == 1 ? '0' : $data['temp1']) .
+      ' HEATER_0_MAXTEMP='    . $data['maxtemp0'] .
+      ' HEATER_1_MAXTEMP='    . $data['maxtemp1'] .
+      ' INVERT_X_DIR='        . ($data['invertX'] == 0 ? 'false' : 'true') .
+      ' INVERT_Y_DIR='        . ($data['invertY'] == 0 ? 'false' : 'true') .
+      ' LANGUAGE_CHOICE='     . $data['language']
+      ;
+    if ($data['target'] == 'HBK') {
+      $cmd .= ' ULTIMAKER_HBK';
+    }
+    if ($data['controller'] == 'FGSC') {
+      $cmd .= ' REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER NO_ULTIMAKERCONROLLER';
+    }
+    if ($data['timeout'] != 15000) {
+      $cmd .= ' LCD_TIMEOUT_TO_STATUS=' . $data['timeout'];
+    }
+    if ($data['displayFan'] == 1) {
+      $cmd .= ' DISPLAY_FAN';
+    }
+    if ($data['pulleys'] == 1) {
+      $cmd .= ' ULTIMAKER_GT2';
+    }
+    if ($data['beep'] == 'Marlin') {
+      $cmd .= ' LCD_FEEDBACK_FREQUENCY_HZ=5000 LCD_FEEDBACK_FREQUENCY_DURATION_MS=2';
+    }
+    if ($data['reverseEncoder'] == 1) {
+      $cmd .= ' REVERSE_ENCODER';
+    }
+    // Close defines
+    $cmd .= '" ';
+
+    // Capture errors and don't leak unnecessary server info
+    $cmd .= '2>&1 | sed -e "s!' . ARDUINO_PATH . '!ARDUINO_PATH!g" -e "s!' . $buildDir . '!BUILD_DIR!g" ';
+
+    // Build the firmware!
+    $resp['log'] = shell_exec($cmd);
+    // $resp['log'] .= "\n" . $cmd;
+
+    $firmware = $buildDir . '/' . FIRMWARE;
+  
+    // Ensure we have something to return
+    if (!file_exists($firmware)) {
+      exec('rm -rf "' . $buildDir . '"');
+      goto exit_json;
+    }
+
+    // Load file and compute MD5 hash
+    $resp['firmware'] = file_get_contents($firmware);
+    $resp['md5'] = md5($resp['firmware']);
+
+    // Cleanup
+    exec('rm -rf "' . $buildDir . '"');
+  
+    // Call it a success
+    $resp['status']     = 0;
+    break;
+  default:
+    // Unknown request
+    goto exit_json;
+}
+    
+exit_json:
+
+echo json_encode($resp);
+
+?>
